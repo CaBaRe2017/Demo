4dbbc598fde6d61f9d0b7f3c19dd8673bb84acd0
GreySlon <garanenkosergii@gmail.com>
2017-08-25 15:18:35 +0300

    MainController refactoring

--- src/main/java/com/ua/cabare/MainController.java
+++ src/main/java/com/ua/cabare/MainController.java
@@ -1,47 +1,41 @@
 package com.ua.cabare;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.ua.cabare.models.Dish;
 
-@Controller
-@RequestMapping(path="/menu")
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/menu")
 public class MainController {
 
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired
+  private UserRepository userRepository;
 
-    @GetMapping(path = "/add")
-    public @ResponseBody
-    String addNewDish(@RequestParam String name
-            , @RequestParam Double price) {
+  @RequestMapping("/add")
+  public String addNewDish(@RequestParam String name, @RequestParam Double price) {
 
-        Dish d = new Dish();
-        d.setName(name);
-        d.setPrice(price);
-        userRepository.save(d);
-        return "Complete";
-    }
+    Dish dish = new Dish();
+    dish.setName(name);
+    dish.setPrice(price);
+    userRepository.save(dish);
+    return "Complete";
+  }
 
-    @GetMapping(path = "/all")
-    public @ResponseBody
-    Iterable<Dish> getAllDishes() {
-        return userRepository.findAll();
-    }
+  @RequestMapping("/all")
+  public Iterable<Dish> getAllDishes() {
+    return userRepository.findAll();
+  }
 
-    @GetMapping(path = "/get")
-    public @ResponseBody
-    Dish getOneDish(Long id) {
-        return userRepository.findOne(id);
-    }
+  @RequestMapping("/get")
+  public Dish getOneDish(Long id) {
+    return userRepository.findOne(id);
+  }
 
-    //@GetMapping(path = "/menu")
-    //public @ResponseBody Iterable<Dish> getAllByColumn(String name) {
-    //return userRepository.findAll(name);
+  //@GetMapping(path = "/menu")
+  //public @ResponseBody Iterable<Dish> getAllByColumn(String name) {
+  //return userRepository.findAll(name);
 
 }
