d988643aa73da2ada35d027e7708a302fdd2512a
GreySlon <garanenkosergii@gmail.com>
2017-10-25 20:43:07 +0300

    added dish repository

--- src/main/java/com/ua/cabare/repositiries/DishRepository.java
+++ src/main/java/com/ua/cabare/repositiries/DishRepository.java
@@ -2,11 +2,19 @@ package com.ua.cabare.repositiries;
 
 import com.ua.cabare.models.Dish;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DishRepository extends CrudRepository<Dish, Long> {
 
   Optional<Dish> findById(long id);
+
+  Dish save(Dish dish);
+
+  @Query("SELECT d FROM Dish d")
+  List<Dish> streamAllPaged(Pageable pageable);
 }
